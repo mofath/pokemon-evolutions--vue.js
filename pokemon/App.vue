48fdcc45afbe5fa011b61b50266137989d4a410b
mofath <mohamed.elgaedi@gmail.com>
2021-02-27 18:50:18 +0200

    Added Card component

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -1,41 +1,60 @@
 <template>
-  <div class="card">
-    <div class="title">title</div>
-    <div class="content">content</div>
-    <div class="description">description</div>
+  <div class="cards">
+    <card v-for="starter in starters" >
+      <template v-slot:title>
+        {{ starter.name }}
+      </template>
+      <template v-slot:content>
+        <img :src="starter.sprite" alt="" />
+      </template>
+      <template v-slot:description>
+        <div v-for="type in starter.types">
+          {{ type }}
+        </div>
+      </template>
+    </card>
   </div>
 </template>
-
+ 
 <script>
-export default {};
+import Card from "./card.vue";
+const api = "https://pokeapi.co/api/v2/pokemon";
+const ids = [1, 4, 7];
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      starters: [],
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const responses = await Promise.all(
+        ids.map((id) => window.fetch(`${api}/${id}`))
+      );
+      const data = await Promise.all(responses.map((res) => res.json()));
+      this.starters = data.map((datum) => ({
+        name: datum.name,
+        sprite: datum.sprites.other["official-artwork"].front_default,
+        types: datum.types.map((type) => type.type.name),
+      }));
+      console.log(this.starters);
+    },
+
+  },
+};
 </script>
 
 <style scoped>
-.card {
-  border: 1px solid silver;
-  border-radius: 8px;
-  max-width: 200px;
-  margin: 0 5px;
-  cursor: pointer;
-  box-shadow: 0px 1px 3px darkgray;
-  transition: 0.2s;
+.cards {
+  display: flex;
 }
-.card:hover {
-  transition: 0.2s;
-  box-shadow: 0px 1px 9px darkgray;
-}
-.title,
-.content,
-.description {
-  padding: 16px;
-  text-transform: capitalize;
-  text-align: center;
-}
-.title,
-.content {
-  border-bottom: 1px solid silver !important;
-}
-.title {
-  font-size: 1.25em;
+img {
+  width: 100%;
 }
 </style>
